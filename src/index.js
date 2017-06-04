393d8c1d9e1371ac8c6931557de937546842bf6b
Syed Usman <syedusman.1@gmail.com>
2017-06-04 15:26:03 +1000

    istanbul

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let calculateSum =  function calculateSum(a,b){
+  if (a>100){
+    return 0;
+  }
+
+  return (a+b);
+}
+
+module.exports={
+calculateSum: calculateSum
+}
--- src/test/first.spec.js
+++ src/test/first.spec.js
@@ -1,4 +1,6 @@
 var expect = require ('chai').expect;
+var calculateSum = require('./../index.js').calculateSum;
+console.log(calculateSum);
 require('mocha');
 
 describe("run first test",function(){
