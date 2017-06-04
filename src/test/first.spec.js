5a4ddd8ce9a00ecaa7fd15194f5e6a737f3985df
Syed Usman <syedusman.1@gmail.com>
2017-06-04 10:20:22 +1000

    setting up mocha, chai and path for the folder

---
+++ src/test/first.spec.js
@@ -0,0 +1,9 @@
+var expect = require ('chai').expect;
+require('mocha');
+
+describe("run first test",function(){
+  it('should pass when everything ok',function(){
+    expect(true).to.equal(true);
+  });
+})
+
